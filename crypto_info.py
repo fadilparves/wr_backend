221d869e1d4378c230c4eb63bdc04ffc0377b662
Mohamad Fadil <fadil.parves@gmail.com>
2022-05-22 22:21:28 +0800

    working on the schedule

--- crypto_info.py
+++ crypto_info.py
@@ -5,42 +5,43 @@ import json
 import pandas as pd
 from privateconfig import coinmarketcap_key
 
-rng = [1, 5001, 10001, 15001, 20001, 25001, 30001, 35001, 40001, 45001, 55001, 60001, 65001, 70001, 75001, 80001, 85001, 90001, 95001, 100000]
+def crypto_info():
+    rng = [1, 5001, 10001, 15001, 20001, 25001, 30001, 35001, 40001, 45001, 55001, 60001, 65001, 70001, 75001, 80001, 85001, 90001, 95001, 100000]
 
-rum = []
-for r in rng:
-    start = r
-    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
-    parameters = {
-    'start': start,
-    }
+    rum = []
+    for r in rng:
+        start = r
+        url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
+        parameters = {
+        'start': start,
+        }
 
-    headers = {
-    'Accepts': 'application/json',
-    'X-CMC_PRO_API_KEY': coinmarketcap_key,
-    }
+        headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': coinmarketcap_key,
+        }
 
-    session = Session()
-    session.headers.update(headers)
+        session = Session()
+        session.headers.update(headers)
 
-    try:
-        row = []
-        response = session.get(url, params=parameters)
-        data = json.loads(response.text)
+        try:
+            row = []
+            response = session.get(url, params=parameters)
+            data = json.loads(response.text)
 
-        if 'data' in data:
-            print(start)
-            for d in data['data']:
-                row.append([d['id'], d['symbol']])
-        else:
-            break
+            if 'data' in data:
+                print(start)
+                for d in data['data']:
+                    row.append([d['id'], d['symbol']])
+            else:
+                break
 
-    except (ConnectionError, Timeout, TooManyRedirects) as e:
-        print(e)
+        except (ConnectionError, Timeout, TooManyRedirects) as e:
+            print(e)
 
-    df = pd.DataFrame(row, columns=["ID", "Coin"])
-    rum.append(df)
-    
-df = pd.concat(rum, axis=0)
-df.drop_duplicates(subset=['ID', 'Coin'], keep='first', inplace=True)
-df.to_csv("./data/mapping.csv", index=False)
+        df = pd.DataFrame(row, columns=["ID", "Coin"])
+        rum.append(df)
+        
+    df = pd.concat(rum, axis=0)
+    df.drop_duplicates(subset=['ID', 'Coin'], keep='first', inplace=True)
+    df.to_csv("./data/mapping.csv", index=False)
